96e8a5d669a50765e84288353e3ad1d7b9ae48aa
sessinou <gbenounicolas@gmail.com>
2019-03-22 16:44:27 +0100

    template implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { WorksComponent } from './works/works.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import {  HttpClientModule } from '@angular/common/http';
+import {WorksService} from './services/works.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    WorksComponent,
+    HomeComponent,
+    ServiceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WorksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/works/works.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-works',
+  templateUrl: './works.component.html',
+  styleUrls: ['./works.component.sass']
+})
+export class WorksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
